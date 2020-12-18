b5f70ad798629b8255cfc7249ac08bac02064e5c
Glen <psycotik666@gmail.com>
2020-12-18 16:01:30 +0000

    added a watcher

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 const del = require('del');
-const {src, dest, series} = require('gulp');
+const {src, dest, series, parallel, watch} = require('gulp');
 
 function clean(cb) {
     del('public/**', {force:true});
@@ -26,9 +26,20 @@ function css(cb) {
     cb();
 }
 
+function watcher(cb) {
+    watch('private/index.html').on('change', series(html));
+    watch('private/media/**').on('change', series(media));
+    watch('private/script/**').on('change', series(javascript));
+    watch('private/style/**').on('change', series(css));
+    cb();
+};
+
 exports.clean = clean;
 exports.html = html;
 exports.media = media;
 exports.javascript = javascript;
 exports.css = css;
-exports.default = series(clean, html, media, javascript, css);
+exports.watcher = watcher;
+exports.build = series(clean, parallel(html, media, javascript, css));
+exports.auto = series(exports.build, watcher);
+exports.default = exports.auto;
