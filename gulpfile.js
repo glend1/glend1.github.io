0e06201095d24c85739fba05a4438eb49b023f4b
Glen <psycotik666@gmail.com>
2020-12-23 14:38:09 +0000

    removed todo's and organized gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -14,18 +14,32 @@ function clean(cb) {
 
 function html(cb) {
     src('private/index.html')
-        //TODO add the ability to pass in a custom reporter only
-        .pipe(htmllint(htmllintReporter))
-        //TODO add the ability to pass in a file configuration
-        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(htmllint({
+            "line-end-style": false,
+            "id-class-style": "dash"
+        }, htmllintReporter))
+        .pipe(htmlmin({ 
+            "collapseWhitespace": true 
+        }))
         .pipe(dest('public'));
     cb();
 }
 
+function htmllintReporter(filepath, issues) {
+	if (issues.length > 0) {
+		issues.forEach(function (issue) {
+			fancyLog(colors.cyan('[gulp-htmllint] ') + colors.white(filepath + ' [' + issue.line + ',' + issue.column + ']: ') + colors.red('(' + issue.code + ') ' + issue.msg));
+		});
+
+		process.exitCode = 1;
+	}
+};
+
 function media(cb) {
     src('private/media/**')
-        //TODO add the ability to pass in a file configuration
-        .pipe(imagemin({"silent": true}))
+        .pipe(imagemin({
+            "silent": true
+        }))
         .pipe(dest('public/media'));
     cb();
 }
@@ -56,16 +70,6 @@ function reload(cb) {
         browserSync.reload()
     };
     cb()
-};
-
-function htmllintReporter(filepath, issues) {
-	if (issues.length > 0) {
-		issues.forEach(function (issue) {
-			fancyLog(colors.cyan('[gulp-htmllint] ') + colors.white(filepath + ' [' + issue.line + ',' + issue.column + ']: ') + colors.red('(' + issue.code + ') ' + issue.msg));
-		});
-
-		process.exitCode = 1;
-	}
 };
 
 function watcher(cb) {
